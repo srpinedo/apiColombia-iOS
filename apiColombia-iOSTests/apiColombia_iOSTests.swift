7e9260546a2bce6acc21eb8f3e1ae1bf36227f68
Sebastian Pinedo <100978175+srpinedo@users.noreply.github.com>
2026-02-03 21:45:55 -0500

    Initial Commit

---
+++ apiColombia-iOSTests/apiColombia_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  apiColombia_iOSTests.swift
+//  apiColombia-iOSTests
+//
+//  Created by Joan on 3/02/26.
+//
+
+import Testing
+@testable import apiColombia_iOS
+
+struct apiColombia_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ apiColombia-iOSUITests/apiColombia_iOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  apiColombia_iOSUITestsLaunchTests.swift
+//  apiColombia-iOSUITests
+//
+//  Created by Joan on 3/02/26.
+//
+
+import XCTest
+
+final class apiColombia_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
